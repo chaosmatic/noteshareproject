d55095cea88e94b6c89b6336383b5f4a5a666022
Daniel Tang <dt.tangr@gmail.com>
2012-02-28 19:52:39 +1100

    added database access code

---
+++ config.php
@@ -0,0 +1,23 @@
+<?php
+error_reporting(E_ALL ^ E_NOTICE);
+define("DB_DSN","mysql:host=localhost;dbname=db");
+define("DB_USR","user!");
+define("DB_PWD","password!");
+
+/*Kill them magic quotes*/
+if (get_magic_quotes_gpc()) {
+    $process = array(&$_GET, &$_POST, &$_COOKIE, &$_REQUEST);
+    while (list($key, $val) = each($process)) {
+        foreach ($val as $k => $v) {
+            unset($process[$key][$k]);
+            if (is_array($v)) {
+                $process[$key][stripslashes($k)] = $v;
+                $process[] = &$process[$key][stripslashes($k)];
+            } else {
+                $process[$key][stripslashes($k)] = stripslashes($v);
+            }
+        }
+    }
+    unset($process);
+}
+?>
