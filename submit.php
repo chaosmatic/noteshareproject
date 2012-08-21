08ef1bd138787d63f03fd5f4eb96ba8bbc4c71e9
Jonathan <jonathan.milford@gmail.com>
2012-08-21 21:37:23 +1000

    updated submit

--- submit.php
+++ submit.php
@@ -2,6 +2,7 @@
 //TODO: SUBMIT VALUES TO DB ON UPLOAD
 require_once('head.php');
 require_once ('database.php');
+echo "<a href='index.php'>Home</a>";
 //SANITIZATION
 $subject = filter_var($_POST["subject"], FILTER_SANITIZE_STRING);//NEED TO SANITIZE AND VALIDATE THESE GUYS
 $title = filter_var($_POST["title"], FILTER_SANITIZE_STRING);
@@ -11,7 +12,7 @@ $author = filter_var($_POST["author"], FILTER_SANITIZE_STRING);
 $filename = $_FILES['datafile']['name']; //debugging only
 $ext = pathinfo($_FILES['datafile']['name'], PATHINFO_EXTENSION);
 $ext = filter_var($ext, FILTER_SANITIZE_STRING);
-
+echo $ext;
 //VALIDATION
 $allowedsubject = array("Math","Physics","Chemistry","Biology","English","Literature","Spesh");
 if (in_array($subject, $allowedext)){
@@ -22,7 +23,7 @@ if (in_array($ext, $allowedext)){
 	$validext = True;
 }
 if (strlen($title)<=12 && strlen($topic)<=12 && strlen($author)<=12){
-	$validtta = True:
+	$validtta = True;
 }
 
 
@@ -30,7 +31,6 @@ $dbh = new databaseaccess;
 $uploaddir = '/var/uploads/';
 $filename = hash_file('sha256',($_FILES['datafile']['tmp_name'])) .".".$ext;
 $uploadfile = $uploaddir . $filename;
-
 if ($allowedext && $allowedsubject && $validtta && null != $subject && strlen($topic)>0 && strlen($author)>0 && strlen($filename)>0){
 	if (move_uploaded_file($_FILES['datafile']['tmp_name'], $uploadfile)) {
 			echo "File is valid, and was successfully uploaded.\n";
